7bd3899380317fcddaab9969676f71160cbf60ee
Bruno_Ss <brunosch250@gmail.com>
2025-11-14 18:03:50 -0300

    Created Tabs Component

---
+++ src/components/Tabs/index.jsx
@@ -0,0 +1,17 @@
+import styles from "./Tabs.module.css";
+
+export default function Tabs({ tabs, activeTab, onChange }) {
+  return (
+    <div className={styles.tabsContainer}>
+      {tabs.map((tab) => (
+        <button
+          key={tab}
+          className={`${styles.tabButton} ${activeTab === tab ? styles.active : ""}`}
+          onClick={() => onChange(tab)}
+        >
+          {tab}
+        </button>
+      ))}
+    </div>
+  );
+}
