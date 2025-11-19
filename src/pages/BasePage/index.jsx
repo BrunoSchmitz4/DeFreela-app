4d02d917f42824752060498219320a6e16dd7364
Bruno_Ss <brunosch250@gmail.com>
2025-11-19 20:25:49 -0300

    Update BasePage

--- src/pages/BasePage/index.jsx
+++ src/pages/BasePage/index.jsx
@@ -1,7 +1,13 @@
 import { Outlet } from 'react-router';
 import Header from '../../components/Header';
+import { useAuth } from '../../context/authContext';
+import AuthLoadingScreen from '../../components/ui/AuthLoadingScreen';
 
 function BasePage() {
+    const { loading } = useAuth();
+
+    if (loading) return <AuthLoadingScreen />
+
     return (
         <>
             <Header />
